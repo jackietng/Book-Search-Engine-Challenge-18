5260bd33436bc6d5821c0616b7a37d59e2689240
jackietng <jn1thythy56@gmail.com>
2025-02-04 16:03:37 -0700

    updated frontend files for graphql

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5173,
     open: true,
     proxy: {
-      '/api': {
+      '/graphql': {
         target: 'http://localhost:3001',
+        changeOrigin: true,
         secure: false,
-        changeOrigin: true
       }
     }
   }
